302ca20a5eaf51c196e29c2fabefc2812fc92b05
sainath <nagasivasai8910@gmail.com>
2026-07-08 10:36:26 +0530

    no auth

--- services/render/__init__.py
+++
@@ -1,8 +0,0 @@
-# services/render package initializer
-from .renderer import render_section, RenderDescriptor, get_renderer_for_type
-
-__all__ = [
-    "render_section",
-    "RenderDescriptor",
-    "get_renderer_for_type",
-]
